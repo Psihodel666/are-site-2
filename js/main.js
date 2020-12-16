2923a6354639191de95804b7743294286fe85be3
Psihodel <grant10005@gmail.com>
2020-12-16 21:51:55 +0200

    are site

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+$('.menu-btn').on('click', function(e) {
+e.preventDefault;
+$(this).toggleClass('menu-btn_active');
+});
+});
+
+$(document).ready(function () {
+
+        $('.menu').click(function() {
+            $('.overlay').toggleClass('anim');
+            $(this).addClass('open')
+        });
+
+        $('.open').click(function(){
+            $('.overlay').toggleClass('reverse-animation');
+        })
+});
+
+
+
+
+
+
+
+'use strict';
+
+(function() {
+  var body = document.body;
+  var burgerMenu = document.getElementsByClassName('b-menu')[0];
+  var burgerContain = document.getElementsByClassName('b-container')[0];
+  var burgerNav = document.getElementsByClassName('b-nav')[0];
+
+  burgerMenu.addEventListener('click', function toggleClasses() {
+    [body, burgerContain, burgerNav].forEach(function (el) {
+      el.classList.toggle('open');
+    });
+  }, false);
+})();
